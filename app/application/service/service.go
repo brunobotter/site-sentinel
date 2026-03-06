91f6f5d74ba3240f27610a02c77017ff17af9d88
brunobotter <62908563+brunobotter@users.noreply.github.com>
2026-03-06 18:21:43 -0300

    feat: interfaces

--- app/application/service/service.go
+++ app/application/service/service.go
@@ -6,14 +6,24 @@ import (
 	"github.com/brunobotter/site-sentinel/application/domain"
 )
 
+// Ele é responsável por planejar como os checks serão executados.
+// Como devemos organizar os targets para execução?
+// Lista de MonitorTarget e divide em lotes
 type MonitorPlannerService interface {
 	PlanBatch(targets []domain.MonitorTarget) [][]domain.MonitorTarget
 }
+
+// Esse serviço trabalha depois da execução dos checks.
+// resultados de monitoramento
+// O CheckAggregationService transforma dados brutos em métricas.
 type CheckAggregationService interface {
+	//Calcular uptime
 	CalculateUptime(results []domain.CheckResult) float64
 
+	//calcular latencia media
 	AverageLatency(results []domain.CheckResult) float64
 
+	//Agrupar resultados por janela de tempo
 	GroupByWindow(
 		results []domain.CheckResult,
 		window time.Duration,
