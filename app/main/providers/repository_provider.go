fd5018893d33f74df4ea4353ded5e0842029d85a
brunobotter <62908563+brunobotter@users.noreply.github.com>
2026-03-06 21:49:45 -0300

    fet provider

--- app/main/providers/repository_provider.go
+++ app/main/providers/repository_provider.go
@@ -1,7 +1,11 @@
 package providers
 
 import (
+	"github.com/brunobotter/site-sentinel/application/repo"
+	"github.com/brunobotter/site-sentinel/infra/logger"
+	infraRepo "github.com/brunobotter/site-sentinel/infra/repo"
 	"github.com/brunobotter/site-sentinel/main/container"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type RepositoryProvider struct{}
@@ -10,5 +14,11 @@ func NewRepositoryProvider() *RepositoryProvider {
 	return &RepositoryProvider{}
 }
 func (p *RepositoryProvider) Register(c container.Container) {
+	c.Singleton(func(db *pgxpool.Pool, log logger.Logger) repo.MonitorTargetRepository {
+		return infraRepo.NewMonitorTargetPgRepository(db, log)
+	})
 
+	c.Singleton(func(db *pgxpool.Pool, log logger.Logger) repo.CheckResultRepository {
+		return infraRepo.NewCheckResultPgRepository(db, log)
+	})
 }
